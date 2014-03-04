bac7abdf8c469d495444733a46d82a1ae258e2a2
Nagy Gábor <ng.murdock@gmail.com>
2014-03-04 21:02:45 +0100

    Update_v4

--- ConnectingCompanies/Forms/NewMailForm.Designer.cs
+++ ConnectingCompanies/Forms/NewMailForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // NewMailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "NewMailForm";
+            this.ClientSize = new System.Drawing.Size(384, 311);
+            this.Name = "NewMailForm";
+            this.Text = "Új üzenet";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
